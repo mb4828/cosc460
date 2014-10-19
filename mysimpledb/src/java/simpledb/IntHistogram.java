a0b31a4bcbdbfe16de9f6831081e73964aab82d3
Matt Brauner <mbrauner@colgate.edu>
2014-10-19 11:24:39 -0400

    Lab 4 in progress

--- mysimpledb/src/java/simpledb/IntHistogram.java
+++ mysimpledb/src/java/simpledb/IntHistogram.java
@@ -4,7 +4,12 @@ package simpledb;
  * A class to represent a fixed-width histogram over a single integer-based field.
  */
 public class IntHistogram {
-
+	private int[] hist;			// contains the histogram buckets
+	private int hmax;			// maximum value in the histogram
+	private int hmin;			// minimum value in the histogram
+	private int hbuckets;		// number of buckets
+	private int htotal;			// total number of elements in the histogram
+	
     /**
      * Create a new IntHistogram.
      * <p/>
@@ -22,18 +27,89 @@ public class IntHistogram {
      * @param max     The maximum integer value that will ever be passed to this class for histogramming
      */
     public IntHistogram(int buckets, int min, int max) {
-        // some code goes here
+    	hmax = max;
+        hmin = min;
+        htotal = 0;
+        
+        // gracefully handle too many buckets
+        if (buckets > (max-min+1))
+        	hbuckets = (max-min+1);
+        else
+        	hbuckets = buckets;
+        
+        hist = new int[hbuckets];
+        
+        for (int i=0; i < hist.length; i++) {
+        	hist[i] = 0;
+        } 
+        
+        System.out.println("NEW HISTOGRAM: buckets=" + hbuckets + ", min=" + hmin + ", max=" + hmax);
     }
 
+    private int whichBucket(int v) {
+    	if (v == hmax)
+    		return hbuckets-1;			// value belongs in the final bucket
+    	return (v-hmin) % hbuckets;
+    }
+    
     /**
      * Add a value to the set of values that you are keeping a histogram of.
      *
      * @param v Value to add to the histogram
      */
     public void addValue(int v) {
-        // some code goes here
+        if (v < hmin || v > hmax)
+        	throw new RuntimeException(v + " is not in the range " + hmin + " to " + hmax);
+        
+        hist[whichBucket(v)] += 1;  
+        htotal++;
+        
+        //System.out.println("inserted value " + v + " into bucket " + whichBucket(v));
     }
-
+    
+    /**
+     * Gets the range of the bucket
+     */
+    private double getWidth(int bucketnum) {
+    	if (bucketnum != (hbuckets-1))
+    		return (hmax-hmin+1) / hbuckets;
+    	else
+    		return (hmax-hmin+1) % hbuckets;
+    }
+    
+    /**
+     * Estimates the frequency of one value in the histogram
+     */
+    private double getBucketSlice(int v) {
+    	int bucketnum = whichBucket(v);
+    	return hist[bucketnum]/getWidth(bucketnum);
+    }
+    
+    /**
+     * Returns the lower bound of the bucket which contains v
+     */
+    private int getLowerBound(int v, int bucketnum) {
+    	int interval = (hmax-hmin+1) % hbuckets;
+    	return hmin + bucketnum*interval;
+    }
+    
+    /**
+     * Returns a count of the values in the histogram strictly less than v
+     */
+    private double getLowerCount(int v) {
+    	double count = 0;
+    	int bucketnum = whichBucket(v);
+    	
+    	// add full buckets to the left of v
+    	for (int i=0; i<bucketnum; i++)
+    		count += hist[i];
+    	
+    	// add partial buckets to the left of v
+    	count += (hist[bucketnum] / getWidth(bucketnum)) * (v - getLowerBound(v, bucketnum));
+    	
+    	return count;
+    }
+    
     /**
      * Estimate the selectivity of a particular predicate and operand on this table.
      * <p/>
@@ -45,16 +121,50 @@ public class IntHistogram {
      * @return Predicted selectivity of this particular operator and value
      */
     public double estimateSelectivity(Predicate.Op op, int v) {
-
-        // some code goes here
-        return -1.0;
+    	double slice = getBucketSlice(v);
+    	
+    	if (op.equals(Predicate.Op.EQUALS)) {
+    		// prediate is equals
+    		return slice/htotal;
+    	}
+    	if (op.equals(Predicate.Op.NOT_EQUALS)) {
+    		// predicate is not equals
+    		return (htotal - slice) / htotal;
+    	}
+    	
+    	double fcount = 0;
+    	double lowercount = getLowerCount(v);
+    	
+    	if (op.equals(Predicate.Op.LESS_THAN) || op.equals(Predicate.Op.LESS_THAN_OR_EQ)) {
+    		// add lower count, save slice for later
+    		fcount += lowercount;
+    	} else if (op.equals(Predicate.Op.GREATER_THAN) || op.equals(Predicate.Op.GREATER_THAN_OR_EQ)) {
+    		// add inverse of lower count, save slice for later
+    		fcount += htotal - lowercount;
+    	}
+    	
+    	if (op.equals(Predicate.Op.LESS_THAN_OR_EQ) || op.equals(Predicate.Op.GREATER_THAN_OR_EQ)) {
+    		// add slice in if necessary
+    		fcount += slice;
+    	}
+    	
+    	return fcount / htotal;
     }
 
     /**
      * @return A string describing this histogram, for debugging purposes
      */
     public String toString() {
-        // some code goes here
-        return null;
+    	String output = new String();
+    	output += "{" + hmin + ", " + hist[0] + "} ";
+    	
+    	int interval = (hmax-hmin+1) % hbuckets;
+        
+        for (int i=1; i < hist.length; i++) {
+        	output += "{" + (hmin + i*interval) + ", " + hist[i] + "} ";
+        }
+    	
+    	return output;
     }
+    
 }
